4b4d3c753847020121073952bd6205d51fecdac3
Igor Alekseenko <fastedx@gmail.com>
2017-06-22 19:27:38 +0300

    fixes for interface

---
+++ web/admin-panel.php
@@ -0,0 +1,9 @@
+<?php
+use yii\helpers\Html;
+
+?>
+
+<h1>Админка</h1>
+<div class="container">
+    <p><?= yii\helpers\Html::a('Юзеры', ['user/view/index'], ['class' => 'btn btn-success'])?></p>
+</div>
