6c626afe7c14b1c596532e6ff274a223ac21a547
velor <v.khalilov@zonatelecom.ru>
2017-06-28 16:57:50 +0300

    add some sync

--- controllers/SyncController.php
+++ controllers/SyncController.php
@@ -7,17 +7,31 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 
 use app\models\Shop;
+use app\models\Event;
+use app\models\Morda;
+use app\models\Currbatch;
 
 
 class SyncController extends Controller {
 
     public function actionGetShop() {
-        $data = Shop::find()->all();
+        $data = Shop::find()->where(["isdeleted" => "0"])->all();
 
         return $this->asJson($data);
     }
 
     public function actionGetEvents() {
-        
+        $data = Event::find()->where(["isdeleted" => "0"])->all();
+
+        return $this->asJson($data);
     }
+
+    public function actionGetMorda() {
+        $batch = Currbatch::find()->one();
+
+        //$data = Morda::find()->where(["isdeleted" => "0"])->all();
+
+        return $this->asJson($batch);
+    }
+    
 }
