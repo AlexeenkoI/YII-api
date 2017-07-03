d6c1039252b9d49531d7b6d6fdce798ba06dff87
velor <v.khalilov@zonatelecom.ru>
2017-07-04 00:22:29 +0300

    Change user

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -89,6 +89,9 @@ class UserController extends Controller
     {
         $model = $this->findModel($id);
 
+        if ($model->rfcid == "0")
+            $model->rfcid = NULL;
+
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
