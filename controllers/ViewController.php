e503bfb9c76403846e388bfc2597bcc8c7598fd9
Velor <v.khalilov@zonatelecom.ru>
2017-07-04 10:55:11 +0300

    Add order to kubik

--- controllers/ViewController.php
+++ controllers/ViewController.php
@@ -37,7 +37,8 @@ class ViewController  extends Controller {
                                                     IF(ISNULL(p2), 0, p2) as p2,
                                                     IF(ISNULL(p3), 0, p3) as p3
                                                     from `group`
-                                                    left join (select * from grouppriority where batchid = (select currentbatch from currbatch limit 1)) as t on `group`.id = t.groupid")->queryAll());
+                                                    left join (select * from grouppriority where batchid = (select currentbatch from currbatch limit 1)) as t on `group`.id = t.groupid
+                                                    order by (p1 + p2 + p3) desc")->queryAll());
     }
 
 }
