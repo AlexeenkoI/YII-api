f49924807da72359edefefa33788b354dd8c4c8d
Igor Alekseenko <fastedx@gmail.com>
2017-07-08 12:06:41 +0300

    update

--- views/route/update.php
+++ views/route/update.php
@@ -6,9 +6,9 @@ use yii\helpers\Html;
 /* @var $model app\models\Route */
 
 $this->title = 'Update Route: ' . $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Routes', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+// $this->params['breadcrumbs'][] = ['label' => 'Routes', 'url' => ['index']];
+// $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+// $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="route-update">
 
